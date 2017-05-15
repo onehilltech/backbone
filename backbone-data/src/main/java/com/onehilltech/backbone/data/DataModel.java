77f436ea95dca40a39d82f170eccaab3182fad35
James H. Hill <hilljh82@gmail.com>
2017-05-14 20:15:19 -0400

    Converted DataModel to interface; defined BaseDataModel

--- backbone-data/src/main/java/com/onehilltech/backbone/data/DataModel.java
+++ backbone-data/src/main/java/com/onehilltech/backbone/data/DataModel.java
@@ -1,30 +1,7 @@
 package com.onehilltech.backbone.data;
 
-import com.raizlabs.android.dbflow.annotation.PrimaryKey;
-import com.raizlabs.android.dbflow.structure.BaseModel;
-
-public abstract class DataModel extends BaseModel
+public interface DataModel
 {
-  @PrimaryKey
-  public String _id;
-
-  protected DataModel ()
-  {
-
-  }
-
-  protected DataModel (String id)
-  {
-    this._id = id;
-  }
-
-  public String getId ()
-  {
-    return this._id;
-  }
-
-  void setId (String id)
-  {
-    this._id = id;
-  }
+  String getId ();
+  void setId (String id);
 }
