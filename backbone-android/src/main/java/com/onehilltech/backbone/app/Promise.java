fcded01905e0287f0fc5e49d1e4c8bdb686bf4fd
James H. Hill <hilljh82@gmail.com>
2017-06-09 22:41:31 -0400

    Promise does not need to call resolve/reject on same thread that called execute()

--- backbone-android/src/main/java/com/onehilltech/backbone/app/Promise.java
+++ backbone-android/src/main/java/com/onehilltech/backbone/app/Promise.java
@@ -1,5 +1,8 @@
 package com.onehilltech.backbone.app;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,21 +20,24 @@ public class Promise <T>
     void onRejected (Throwable reason);
   }
 
-  private T resolved_;
+  private T resolve_;
 
-  private Throwable rejected_;
+  private Throwable rejection_;
 
   private OnResolved <T> onResolved_;
 
   private OnRejected onRejected_;
 
-  private static final ExecutorService PROMISE_EXECUTOR = Executors.newCachedThreadPool ();
+  private static final ExecutorService DEFAULT_EXECUTOR = Executors.newCachedThreadPool ();
 
   private final PromiseFulfill<T> impl_;
+
+  private final Executor executor_;
 
   public Promise (PromiseFulfill<T> impl)
   {
     this.impl_ = impl;
+    this.executor_ = DEFAULT_EXECUTOR;
   }
 
   /**
@@ -51,25 +57,25 @@ public class Promise <T>
     this.onRejected_ = onRejected;
 
     // Place the promise on the executor.
-    PROMISE_EXECUTOR.execute (this);
+    this.executor_.execute (this);
   }
 
   @Override
   public void run ()
   {
-    if (this.resolved_ != null)
+    if (this.resolve_ != null)
     {
       // The promise has already been resolved. Let's go ahead and pass the value
       // to the caller.
       if (this.onResolved_ != null)
-        this.onResolved_.onResolved (this.resolved_);
+        this.onResolved_.onResolved (this.resolve_);
     }
-    else if (this.rejected_ != null)
+    else if (this.rejection_ != null)
     {
       // The promise has already been rejected. Let's go ahead and pass the reason back
       // to the caller.
       if (this.onRejected_ != null)
-        this.onRejected_.onRejected (this.rejected_);
+        this.onRejected_.onRejected (this.rejection_);
     }
     else
     {
@@ -77,15 +83,6 @@ public class Promise <T>
       // before this method return. Failure to do so means the promise was not
       // completed, and in a bad state.
       this.impl_.execute (this.completion_);
-
-      // Call the correct callback method, or throw an exception. The client cannot
-      // handle the exception, which will force a stack trace.
-      if (this.resolved_ != null)
-        this.onResolved_.onResolved (this.resolved_);
-      else if (this.rejected_ != null)
-        this.onRejected_.onRejected (this.rejected_);
-      else
-        throw new IllegalStateException ("Execute method must call resolve() or reject()");
     }
   }
 
@@ -96,7 +93,36 @@ public class Promise <T>
    */
   public boolean isPending ()
   {
-    return this.rejected_ == null && this.resolved_ == null;
+    return this.rejection_ == null && this.resolve_ == null;
+  }
+
+  /**
+   * Fulfill all the promises.
+   *
+   * @param promises
+   * @return
+   */
+  public static Promise <Collection <Object>> all (Promise... promises)
+  {
+    return new Promise<> ((fulfill) -> {
+      ArrayList <Object> results = new ArrayList<> (promises.length);
+
+      for (Promise <?> promise : promises) {
+        // Execute the promise.
+        promise.run ();
+
+        // Extract the result from the promise, and then add it
+        if (promise.resolve_ != null)
+        {
+
+        }
+        else if (promise.rejection_ != null)
+        {
+          // This promise failed.
+          break;
+        }
+      }
+    });
   }
 
   /**
@@ -111,7 +137,13 @@ public class Promise <T>
       if (!isPending ())
         throw new IllegalStateException ("Promise already resolved/rejected");
 
-      resolved_ = value;
+      // Store the value, and notify the client.
+      resolve_ = value;
+
+      executor_.execute (() -> {
+        if (onResolved_ != null)
+          onResolved_.onResolved (resolve_);
+      });
     }
 
     @Override
@@ -121,7 +153,13 @@ public class Promise <T>
       if (!isPending ())
         throw new IllegalStateException ("Promise already resolved/rejected");
 
-      rejected_ = reason;
+      // Store the reason, and notify the client.
+      rejection_ = reason;
+
+      executor_.execute (() -> {
+        if (onRejected_ != null)
+          onRejected_.onRejected (rejection_);
+      });
     }
   };
 }
