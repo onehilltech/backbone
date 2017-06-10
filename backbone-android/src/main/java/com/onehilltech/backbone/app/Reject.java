156f68bf953447e0e621e8c363083aa0d382c24c
James H. Hill <hilljh82@gmail.com>
2017-06-09 21:38:18 -0400

    Removed unused interfaces

--- backbone-android/src/main/java/com/onehilltech/backbone/app/Reject.java
+++
@@ -1,9 +0,0 @@
-package com.onehilltech.backbone.app;
-
-/**
- * Created by hilljh on 6/9/17.
- */
-public interface Reject
-{
-  void reject (Throwable reason);
-}
--- backbone-android/src/main/java/com/onehilltech/backbone/app/Resolve.java
+++
@@ -1,9 +0,0 @@
-package com.onehilltech.backbone.app;
-
-/**
- * Created by hilljh on 6/9/17.
- */
-public interface Resolve
-{
-  void resolve (Object value);
-}
