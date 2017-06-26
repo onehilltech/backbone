379035a3f673303de8c5c969f1130807c8cc97ce
James H. Hill <hilljh82@gmail.com>
2017-06-26 08:07:37 -0400

    Added LICENSE to top of files

--- backbone-android/src/main/java/com/onehilltech/backbone/app/RejectedOnUIThread.java
+++ backbone-android/src/main/java/com/onehilltech/backbone/app/RejectedOnUIThread.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2017 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.onehilltech.backbone.app;
 
 import android.support.annotation.NonNull;
@@ -15,7 +31,7 @@ public class RejectedOnUIThread extends OnUIThread
    * @param onRejected      The real handler
    * @return                Promise.OnRejected object
    */
-  public static Promise.OnRejected rejectOnUiThread (Promise.OnRejected onRejected)
+  public static Promise.OnRejected rejectOnUiThread (@NonNull Promise.OnRejected onRejected)
   {
     return new RejectedOnUIThread (onRejected);
   }
