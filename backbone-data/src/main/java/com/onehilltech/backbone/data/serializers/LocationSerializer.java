1992f73c77001e739f7e5551338ce362fb00a928
James H. Hill <hilljh82@gmail.com>
2017-07-20 07:51:24 -0400

    Added a Location serializer

---
+++ backbone-data/src/main/java/com/onehilltech/backbone/data/serializers/LocationSerializer.java
@@ -0,0 +1,75 @@
+package com.onehilltech.backbone.data.serializers;
+
+import android.location.Location;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.raizlabs.android.dbflow.converter.TypeConverter;
+
+import java.lang.reflect.Type;
+import java.util.Locale;
+
+@com.raizlabs.android.dbflow.annotation.TypeConverter
+public class LocationSerializer extends TypeConverter <String, Location>
+  implements JsonDeserializer <Location>, JsonSerializer <Location>
+{
+  @Override
+  public String getDBValue (Location model)
+  {
+    return model != null ? String.format (Locale.ENGLISH, "%f,%f", model.getLatitude (), model.getLongitude ()) : null;
+  }
+
+  @Override
+  public Location getModelValue (String data)
+  {
+    if (data == null)
+      return null;
+
+    String [] coordinates = data.split (",");
+    double latitude = Double.parseDouble (coordinates[0]);
+    double longitude = Double.parseDouble (coordinates[1]);
+
+    Location location = new Location ("");
+    location.setLatitude (latitude);
+    location.setLongitude (longitude);
+
+    return location;
+  }
+
+  @Override
+  public Location deserialize (JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+  {
+    if (!json.isJsonArray ())
+      throw new JsonParseException ("The location must be an array.");
+
+    JsonArray jsonLocation = json.getAsJsonArray ();
+
+    if (jsonLocation.size () != 2)
+      throw new JsonParseException ("The location array must contain two elements.");
+
+    double longitude = jsonLocation.get (0).getAsDouble ();
+    double latitude = jsonLocation.get (1).getAsDouble ();
+
+    Location location = new Location ("");
+    location.setLatitude (latitude);
+    location.setLongitude (longitude);
+
+    return location;
+  }
+
+  @Override
+  public JsonElement serialize (Location src, Type typeOfSrc, JsonSerializationContext context)
+  {
+    JsonArray jsonLocation = new JsonArray ();
+    jsonLocation.add (new JsonPrimitive (src.getLongitude ()));
+    jsonLocation.add (new JsonPrimitive (src.getLatitude ()));
+
+    return jsonLocation;
+  }
+}
