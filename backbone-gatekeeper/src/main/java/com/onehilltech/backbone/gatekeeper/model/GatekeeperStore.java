91bdfa0d089b4c110b0b2e5ccb9e4f1f1feff95e
James H. Hill <hilljh82@gmail.com>
2017-11-09 22:56:34 -0500

    Added support for using client with datastore

--- backbone-gatekeeper/src/main/java/com/onehilltech/backbone/gatekeeper/model/GatekeeperStore.java
+++ backbone-gatekeeper/src/main/java/com/onehilltech/backbone/gatekeeper/model/GatekeeperStore.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import com.onehilltech.backbone.data.DataStore;
 import com.onehilltech.backbone.data.DataStoreAdapter;
 import com.onehilltech.backbone.data.serializers.ObjectIdSerializer;
-import com.onehilltech.backbone.objectid.ObjectId;
+import com.onehilltech.backbone.gatekeeper.GatekeeperClient;
 import com.onehilltech.backbone.gatekeeper.GatekeeperSessionClient;
+import com.onehilltech.backbone.objectid.ObjectId;
 
 import okhttp3.CacheControl;
 
@@ -34,5 +35,15 @@ public class GatekeeperStore
         .build ();
 
     return dataStore_;
+  }
+
+  public static DataStore getInstance (Context context, GatekeeperClient client)
+  {
+    return new DataStore.Builder (context, GatekeeperDatabase.class)
+        .setBaseUrl (client.getBaseUrlWithVersion ())
+        .setApplicationAdapter (dataStoreAdapter_)
+        .setHttpClient (client.getHttpClient ())
+        .addTypeAdapter (ObjectId.class, new ObjectIdSerializer ())
+        .build ();
   }
 }
